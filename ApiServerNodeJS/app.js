9823ceecfc9420f24e0f1d45fe3c4ecaf1d28fa1
Daniel <felikssolomonov@gmail.com>
2019-07-11 16:44:04 +0300

    mongodb added

--- ApiServerNodeJS/app.js
+++ ApiServerNodeJS/app.js
@@ -1,93 +1,116 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
 const followRouter = express.Router();
 
-// app.delete("/api/follow/:id", function(request, response){
-// 	// not work now
-//   response.header("Access-Control-Allow-Origin", "*");
-// 	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// 	let id = request.params["id"];
-// 	let data = JSON.parse(fs.readFileSync("json/follow.json", "utf8"));
-// 	data.filter(function(it){
-// 		if(it.id == id) {
-// 			it.follow = false;
-// 		}
-// 	});
-// 	fs.writeFileSync("json/follow.json", JSON.stringify(data, "", 2));
-// 	response.send(data);
-// });
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/";
+const mongoClient = new MongoClient(url, {useNewUrlParser: true});
+const dbAPI = "apiSN";
+const tableFollow = "follow";
+const tableProfile = "profile";
+const tableUsers = "users";
+let listener;
+let dbClient;
 
-followRouter.post("/:id", function(request, response){
-	response.header("Access-Control-Allow-Origin", "*");
-	let id = request.params["id"];
-	//users && follow
-	let data = JSON.parse(fs.readFileSync("json/follow.json", "utf8"));
-	data.filter(function(it){
-		if(it.id == id) {
-			it.follow = true;
-		}
-	});
-	fs.writeFileSync("json/follow.json", JSON.stringify(data, "", 2));
-	//
-	let data2 = JSON.parse(fs.readFileSync("json/users.json", "utf8"));
-	data2.items.filter(function(it){
-		if(it.id == id) {
-			it.followed = true;
-		}
-	});
-	fs.writeFileSync("json/users.json", JSON.stringify(data2, "", 2));
-	// {true
-  //   resultCode: 0
-  //   messages: [],
-  //   data: {}
-	// }
-	// {false
-  //   resultCode: 1 // !0
-  //   messages: ['Something wrong'],
-  //   data: {}
-	// }
-	response.send("data");
+mongoClient.connect(function(err, client){
+    if(err) return console.log(err);
+    dbClient = client;
+		app.locals.data = client.db(dbAPI);
 });
 
-followRouter.get("/:id", function(request, response){
-	response.header("Access-Control-Allow-Origin", "*");
-	let id = request.params["id"];
-	let arr = JSON.parse(fs.readFileSync("json/follow.json", "utf8"));
-	let filtered = arr.filter(function(item){
-		if(item.id == id) return item;
-  });
-	filtered = filtered[0].follow;
-	response.send(filtered);
+process.on("SIGINT", () => {
+    dbClient.close();
+    process.exit();
 });
-
-app.use("/api/follow", followRouter);
 
 app.get("/api/users", function(request, response){
 	response.header("Access-Control-Allow-Origin", "*");
 	let page = request.query.page ? request.query.page : 1;
-	let count = request.query.count ? request.query.count : 2;
-	let data = JSON.parse(fs.readFileSync("json/users.json", "utf8"));
-	let res = data;
-	let arr = [];
-	for (let i = (page-1)*count; i < (page*count) && i < data.items.length; i++) {
-		arr.push(data.items[i]);
-	}
-	res.items = arr;
-	response.send(res);
+	let count = request.query.count ? request.query.count : 5;
+	const collection = request.app.locals.data.collection(tableUsers);
+	//-1 по убыванию 1 по возрастанию
+	collection.find({}).sort({_id:-1}).toArray(function(err, users){
+			if(err) return console.log(err);
+      else {
+				let arr = [];
+				for (let i = (page-1)*count; i < (page*count) && i < users.length; i++) {
+					arr.push(users[i]);
+				}
+				// console.log(users.length);
+				response.send({items: arr, totalCount: users.length});
+			}
+    });
 });
 
 app.get("/api/profile/:id", function(request, response){
 	response.header("Access-Control-Allow-Origin", "*");
-	let id = request.params["id"] ? request.params["id"] : 1200;
-	let arr = JSON.parse(fs.readFileSync("json/profile.json", "utf8"));
-	let filtered = arr.filter(function(item){
-		if(item.userId == id) return item;
-  });
-	response.send(filtered[0]);
+	const id = request.params["id"] ? request.params["id"] : 1200;
+	const collection = request.app.locals.data.collection(tableProfile);
+	collection.findOne({_id: +id}, function(err, user){
+        if(err) return console.log(err);
+        response.send(user);
+    });
 });
 
-app.listen(3001, function() {
+app.use("/api/follow", followRouter);
+
+followRouter.get("/:id", function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+	let id = request.params["id"];
+	const collection = request.app.locals.data.collection(tableFollow);
+	collection.findOne({_id: +id}, function(err, user){
+        if(err) return console.log(err);
+        response.send(user.follow);
+    });
+});
+
+followRouter.post("/:id", function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+  console.log("post");
+	let id = request.params["id"];
+	let collection = request.app.locals.data.collection(tableFollow);
+	collection.updateOne({_id: +id}, // критерий выборки
+	      {$set: {follow: true}}, // параметр обновления
+	      function(err, result){
+						if(err) return console.log(err);
+						// response.send(result);
+	      }
+	  );
+  collection = request.app.locals.data.collection(tableUsers);
+  collection.updateOne({_id: +id}, // критерий выборки
+        {$set: {followed: true}}, // параметр обновления
+        function(err, result){
+  					if(err) return console.log(err);
+  					response.send(result);
+        }
+  	);
+});
+
+followRouter.delete("/:id", function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  console.log("delete");
+	let id = request.params["id"];
+	let collection = request.app.locals.data.collection(tableFollow);
+	collection.updateOne({_id: +id}, // критерий выборки
+	      {$set: {follow: false}}, // параметр обновления
+	      function(err, result){
+						if(err) return console.log(err);
+						// response.send(result);
+	      }
+	  );
+  collection = request.app.locals.data.collection(tableUsers);
+	collection.updateOne({_id: +id}, // критерий выборки
+	      {$set: {followed: false}}, // параметр обновления
+	      function(err, result){
+						if(err) return console.log(err);
+						response.send(result);
+	      }
+	  );
+});
+
+listener = app.listen(3001, function() {
 	console.log("Сервер Express");
 	console.log("Прослушивание на порту 3001");
 });
