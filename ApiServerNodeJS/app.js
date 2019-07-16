67735ffbb2d08a6f0588201fbb39a7f97c32e868
Daniel <daniel.solomonov@yandex.ru>
2019-07-16 15:04:48 +0300

    views

--- ApiServerNodeJS/app.js
+++ ApiServerNodeJS/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const followRouter = express.Router();
+const followRouterAPI = express.Router();
+const followRouterJson = express.Router();
 const config = require('./config');
 const dataBase = require("./dataBase");
 const Follow = require("./models/follow");
 const User = require("./models/user");
 const Profile = require("./models/profile");
+const bodyParser = require("body-parser");
 
 const listener = () => {
   app.listen(config.PORT, function() {
@@ -25,6 +27,102 @@ dataBase()
     process.exit(1);
 });
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/json/create", function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+	response.render('create');
+});
+
+app.post("/json/create/profile", function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+  var student = {
+    first: request.body.fname,
+    last: request.body.lname
+  }
+  response.render('created',{userValue : student});
+});
+
+app.get("/json/users", function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+	let page = request.query.page ? request.query.page : 1;
+	let count = request.query.count ? request.query.count : 5;
+	//-1 по убыванию 1 по возрастанию
+	User.find({}).sort({_id:-1}).exec(function(err, res){
+			if(err) return console.log(err);
+      else {
+				let arr = [];
+				for (let i = (page-1)*count; i < (page*count) && i < res.length; i++) {
+					arr.push(res[i]);
+				}
+        response.render('pageUsers', {users: arr});
+			}
+    });
+});
+
+app.get("/json/profile/:id", function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+	const id = request.params["id"] ? request.params["id"] : 1200;
+  Profile.findById(+id, function(err, res){
+        if(err) return console.log(err);
+        response.render('pageProfile', {profile: res});
+    });
+});
+
+app.use("/json/follow", followRouterJson);
+
+followRouterJson.get("/:id", function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+	let id = request.params["id"];
+	Follow.findById(+id, function(err, res){
+        if(err) return console.log(err);
+        response.render('pageFollows', {id: res._id, follow: res.follow});
+    });
+});
+
+followRouterJson.post("/:id", function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+  console.log("post");
+	let id = request.params["id"];
+	Follow.updateOne({_id: +id}, // критерий выборки
+	      {follow: true}, // параметр обновления
+	      function(err, result){
+						if(err) return console.log(err);
+	      }
+	  );
+  User.updateOne({_id: +id},
+        {followed: true},
+        function(err, result){
+  					if(err) return console.log(err);
+  					response.render('pageFollows', {follow: result});
+        }
+  	);
+});
+
+followRouterJson.delete("/:id", function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  console.log("delete");
+	let id = request.params["id"];
+	Follow.updateOne({_id: +id},
+	      {follow: false},
+	      function(err, result){
+						if(err) return console.log(err);
+						// response.send(result);
+	      }
+	  );
+  User.updateOne({_id: +id},
+	      {followed: false},
+	      function(err, result){
+						if(err) return console.log(err);
+						response.render('pageFollows', {follow: result.follow});
+	      }
+	  );
+});
+
+
 app.get("/api/users", function(request, response){
 	response.header("Access-Control-Allow-Origin", "*");
 	let page = request.query.page ? request.query.page : 1;
@@ -37,7 +135,7 @@ app.get("/api/users", function(request, response){
 				for (let i = (page-1)*count; i < (page*count) && i < res.length; i++) {
 					arr.push(res[i]);
 				}
-				response.send({items: arr, totalCount: res.length});
+        response.send({items: arr, totalCount: res.length});
 			}
     });
 });
@@ -51,9 +149,9 @@ app.get("/api/profile/:id", function(request, response){
     });
 });
 
-app.use("/api/follow", followRouter);
+app.use("/api/follow", followRouterAPI);
 
-followRouter.get("/:id", function(request, response){
+followRouterAPI.get("/:id", function(request, response){
 	response.header("Access-Control-Allow-Origin", "*");
 	let id = request.params["id"];
 	Follow.findById(+id, function(err, res){
@@ -62,7 +160,7 @@ followRouter.get("/:id", function(request, response){
     });
 });
 
-followRouter.post("/:id", function(request, response){
+followRouterAPI.post("/:id", function(request, response){
 	response.header("Access-Control-Allow-Origin", "*");
   console.log("post");
 	let id = request.params["id"];
@@ -82,7 +180,7 @@ followRouter.post("/:id", function(request, response){
   	);
 });
 
-followRouter.delete("/:id", function(request, response){
+followRouterAPI.delete("/:id", function(request, response){
 	response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
